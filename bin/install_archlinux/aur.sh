9a6f6345801537debaaa383409b8b8869a2ea06d
ghsable <sn.sable005@gmail.com>
2018-06-11 00:23:07 +0900

    ISSUE#10

--- bin/install_archlinux/aur.sh
+++ bin/install_archlinux/aur.sh
@@ -1,46 +1,17 @@
 #!/bin/bash
 
-echo "${0} ..."
-
-function usage() {
-cat<< _EOT_
-Description:
-  SET XXX
-
-Usage:
-  sh ${0} XXX : xxx
-  sh ${0} XXX : xxx
-  sh ${0} *   : USAGE
-
-EOF:
-  XXX
-
-_EOT_
-exit 1
-}
-
 readonly AUR_DIR=~/aur
 
-case ${1} in
-  mkdir)
-    mkdir -pv ${AUR_DIR}
-    ;;
-  cd)
-    cd ${AUR_DIR}
-    ;;
-  clone)
-    for GITREPOSITORY_URL in $(grep -v -e '^$' -e '^#' $(dirname ${0})/aur.txt)
-    do
-      git clone ${GITREPOSITORY_URL} ${AUR_DIR}
-    done
-    ;;
-  echo)
-    echo $(grep -v -e '^$' -e '^#' $(dirname ${0})/aur.txt)
-    ;;
-  *)
-    usage
-    ;;
-esac
+mkdir -pv ${AUR_DIR}
 
+for GITREPOSITORY_URL in $(grep -v -e '^$' -e '^#' $(dirname ${0})/aur.txt)
+do
+  GITCLONEDIR_NAME=`echo ${GITREPOSITORY_URL} | cut -d "/" -f 4-4 | cut -d "." -f 1-1`
+  git clone ${GITREPOSITORY_URL} ${AUR_DIR}/${GITCLONEDIR_NAME}
+  cd ${AUR_DIR}/${GITCLONEDIR_NAME}
+  vi PKGBUILD
+  makepkg -si
+  cd ..
+done
 
 echo '---------------------------------------->>(EOF)'
