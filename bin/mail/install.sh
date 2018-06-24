bae49bcfa08998f1c96d31bafc07d505c156312a
ghsable <sn.sable005@gmail.com>
2018-06-24 23:42:43 +0900

    ISSUE#10

--- bin/mail/install.sh
+++ bin/mail/install.sh
@@ -10,9 +10,9 @@ Permission:
   File      : 0600
 
 Flow:
-  1. INSTALL PKGS
+  1. INSTALL Pkgs
      $ sh ${0} install
-  2. CREATE DIRECTORY AND FILE
+  2. CREATE Directory/File
      $ mkdir *
      $ vi    *
      Example:
@@ -20,16 +20,16 @@ Flow:
      -> $ sh ${0} example_mra
      -> $ sh ${0} example_mta
      -> $ sh ${0} example_mua
-  3. DEPLOY DIRECTORY AND FILE
+  3. DEPLOY Directory/File
      $ sh ${0} deploy
 
 Usage:
-  sh ${0} install      : INSTALL PKGS
-  sh ${0} deploy       : DEPLOY  CONFIGS
-  sh ${0} example_tree : DISPLAY EXAMPLE_TREE(ALL)
-  sh ${0} example_mra  : DISPLAY EXAMPLE_MRA(POP3)
-  sh ${0} example_mta  : DISPLAY EXAMPLE_MTA(SMTP)
-  sh ${0} example_mua  : DISPLAY EXAMPLE_MUA(MAILER)
+  sh ${0} install      : INSTALL Packages
+  sh ${0} deploy       : DEPLOY  Configs
+  sh ${0} example_tree : DISPLAY Example_tree(ALL)
+  sh ${0} example_mra  : DISPLAY Example_mra(POP3)
+  sh ${0} example_mta  : DISPLAY Example_mta(SMTP)
+  sh ${0} example_mua  : DISPLAY Example_mua(MAILER)
   sh ${0} *            : USAGE
 
 EOF:
@@ -41,6 +41,7 @@ exit 1
  
 function example_tree() {
 cat<< _EOT_
+########## ALL | Directory/File ##########
 Tree Description:
   <HOME>
   ├── <CLOUDSTORAGE_DIR>  // CLOSED //
@@ -92,6 +93,7 @@ Tree Description:
           │   ├── install.sh // ALL
           │   ├── getmail.sh // MRA(rcfile) -> CRONTAB
           │   ├── .muttrc    // MUA
+          │   ├── .mailcap   // MUA
           │   └── .mutt      // MUA
           │       ├── .muttrc_personal
           │       ├── .muttrc_work
@@ -106,21 +108,21 @@ exit 0
 }
 
 function example_mra() {
-########## MRA(Mail Retrieval Agent) | POP3 ##########
 cat<< _EOT_
+########## MRA(Mail Retrieval Agent) | POP3 ##########
 Example(getmailrc_personal):
-  [options]
-  verbose = 0
-  read_all = false
-  [retriever]
-  type = SimplePOP3SSLRetriever
-  server = pop.gmail.com
-  username = xxx@gmail.com
-  port = 995
-  password = xxx
-  [destination]
-  type = Maildir
-  path = ~/.getmail/Maildir/personal/INBOX/
+[options]
+verbose = 0
+read_all = false
+[retriever]
+type = SimplePOP3SSLRetriever
+server = pop.gmail.com
+username = xxx@gmail.com
+port = 995
+password = xxx
+[destination]
+type = Maildir
+path = ~/.getmail/Maildir/personal/INBOX/
 
 _EOT_
 exit 0
@@ -130,30 +132,32 @@ function example_mta() {
 cat<< _EOT_
 ########## MTA(Message Transfer Agent) | SMTP ##########
 Example(.msmtprc):
-  # ~/.msmtprc
-  # echo "<comment>" | msmtp -a <from(account)> <send(address)>
-  # ACCOUNT1 (personal)
-  account        personal
-  host           smtp.gmail.com
-  port           587
-  user           xxx@gmail.com
-  password       xxx
-  from           xxx@gmail.com
-  tls            on
-  tls_starttls   on
-  tls_trust_file /etc/ssl/certs/ca-certificates.crt
-  tls_certcheck  on
-  auth           on
-  logfile        ~/.msmtp.log
-  # ACCOUNT2 (work)
-  account        work
-  host           smtp.xxx.com
-  port           587
-  user           xxx@xxx.com
-  password       xxx
-  from           xxx@xxx.com
-  tls            on
-  ...
+# ~/.msmtprc
+# echo "<comment>" | msmtp -a <from(account)> <send(address)>
+
+# ACCOUNT1 (personal)
+account        personal
+host           smtp.gmail.com
+port           587
+user           xxx@gmail.com
+password       xxx
+from           xxx@gmail.com
+tls            on
+tls_starttls   on
+tls_trust_file /etc/ssl/certs/ca-certificates.crt
+tls_certcheck  on
+auth           on
+logfile        ~/.msmtp.log
+
+# ACCOUNT2 (work)
+account        work
+host           smtp.xxx.com
+port           587
+user           xxx@xxx.com
+password       xxx
+from           xxx@xxx.com
+tls            on
+...
 
 _EOT_
 exit 0
@@ -163,10 +167,10 @@ function example_mua() {
 cat<< _EOT_
 ########## MUA(Mail User Agent) | MAILER ##########
 Example(from_personal):
-  xxx@gmail.com
+xxx@gmail.com
 
 Example(realname_personal):
-  Michael\ Elkins
+Michael\ Elkins
 
 _EOT_
 exit 0
@@ -190,6 +194,7 @@ case ${1} in
     # MUA(Mail User Agent)        | MAILER
     ln -snfv "${CLOUDSTORAGE_DIR}/.mutt_local" "${HOME}"
     ln -snfv "${HOME}/dotfiles/bin/mail/.muttrc" "${HOME}"
+    ln -snfv "${HOME}/dotfiles/bin/mail/.mailcap" "${HOME}"
     ln -snfv "${HOME}/dotfiles/bin/mail/.mutt" "${HOME}"
     ;;
   example_tree | example_mra | example_mta | example_mua)
