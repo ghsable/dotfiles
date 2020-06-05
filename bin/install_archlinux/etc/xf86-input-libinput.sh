d1d92e2a8f70eb76718f119ee485c5b82acf699c
ghsable <sn.sable005@gmail.com>
2020-06-05 14:43:18 +0900

    fix

--- bin/install_archlinux/etc/xf86-input-libinput.sh
+++ bin/install_archlinux/etc/xf86-input-libinput.sh
@@ -9,7 +9,10 @@ sudo pacman -S xf86-input-libinput
 sudo pacman -Sc
 
 # --- setting
-# trackpoint speed
-readonly SYS_FILE="/sys/devices/platform/i8042/serio1/serio2/sensitivity"
-echo '80' | sudo tee ${SYS_FILE}
+# trackpoint speed(udev rules)
+# *sensitivity(default:200) : /sys/devices/platform/i8042/serio1/serio2/sensitivity
+#  speed      (default:97 ) : /sys/devices/platform/i8042/serio1/serio2/speed
+readonly SYS_FILE="/etc/udev/rules.d/10-trackpoint.rules"
+echo 'KERNEL=="serio2", SUBSYSTEM=="serio", DRIVERS=="psmouse", ATTR{sensitivity}:="100", ATTR{speed}:="97"' | sudo tee ${SYS_FILE}
 sudo vi ${SYS_FILE}
+udevadm test /sys/devices/platform/i8042/serio1
