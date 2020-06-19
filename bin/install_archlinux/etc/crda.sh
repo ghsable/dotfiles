7138e71ffd49a56d0f6006a5df5df3102271e6f5
ghsable <sn.sable005@gmail.com>
2020-06-19 12:07:46 +0900

    add crda.sh

---
+++ bin/install_archlinux/etc/crda.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "${0} ..."
+
+# ------ CRDA(Setting up regulation of radio channels for each country) ------
+# --- install(crda)
+sudo pacman -Syu
+sudo pacman -S crda
+sudo pacman -Sc
+
+# -> Error resolution :D
+# platform regulatory.0: Direct firmware load for regulatory.db failed with error -2
+
+# --- setting
+function add_config() {
+cat<< _EOT_
+#################### PLEASE REPLACE THIS LINE ####################
+WIRELESS_REGDOM="JP"
+_EOT_
+return 0
+}
+readonly ETC_FILE="/etc/conf.d/wireless-regdom"
+echo "UPDATE : ${ETC_FILE}"
+add_config | sudo tee -a ${ETC_FILE} >/dev/null
+sudo vi ${ETC_FILE}
