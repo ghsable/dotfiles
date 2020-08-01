0a1b3c29ef10de5f17b973fb0e4b7c48d890229e
ghsable <sn.sable005@gmail.com>
2020-08-01 23:03:45 +0900

    ffmpeg

--- bin/ffmpeg.sh
+++ bin/ffmpeg.sh
@@ -6,9 +6,10 @@ Description:
   CONVERT files
 
 Usage:
-  sh ${0} cut    <hh:dd>-<hh:dd>  <inputfile_path> : CUT     movie_file
-  sh ${0} to-gif <inputfile_path>                  : CONVERT movie_file -> gif_file
-  sh ${0} *                                        : USAGE
+  sh ${0} cut     <hh:dd>-<hh:dd>  <inputfile_path> : CUT     inputfile
+  sh ${0} speedup <inputfile_path>                  : SPEEDUP inputfile
+  sh ${0} to-gif  <inputfile_path>                  : CONVERT inputfile -> gif_file
+  sh ${0} *                                         : USAGE
 
 _EOT_
 exit 1
@@ -20,6 +21,10 @@ case ${1} in
     readonly DATE_YYMMDDHHMM=`date "+%Y%m%d%H%M"`
     cd $(dirname ${3})
     ffmpeg -ss ${2%-*} -i ${3} -t ${2#*-} -vf "scale=${SCALE_VALUE}" -y ffmpeg_${DATE_YYMMDDHHMM}.${3##*.}
+    ;;
+  speedup)
+    readonly SPEED='2.0'
+    ffmpeg -i ${2} -vf setpts=PTS/${SPEED} -af atempo=${SPEED} ${2%.*}_speedup.${2##*.}
     ;;
   to-gif)
     readonly RATE_VALUE='15'
