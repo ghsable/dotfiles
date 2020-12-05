b99950dc64177410cb0d11c246e14f99db42b866
ghsable <sn.sable005@gmail.com>
2020-12-05 23:37:35 +0900

    move package.el to init.el

--- bin/emacs/.emacs.d/init.el
+++ bin/emacs/.emacs.d/init.el
@@ -1,14 +1,25 @@
-;; init.el
+;;; init.el
 
 ;; load-path
 (let ((default-directory (locate-user-emacs-file "./repos")))
   (add-to-list 'load-path default-directory)
   (normal-top-level-add-subdirs-to-load-path))
 
-;; * Common Lisp
-;; Set Steel Bank Common Lisp
+;; package.el
+(require 'package)
+;; override package-archives
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+        ;; ("melpa-stable" . "https://stable.melpa.org/packages/")
+        ("org" . "https://orgmode.org/elpa/")
+        ("gnu" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+;; package-refresh
+(package-refresh-contents)
+
+;; common lisp on Steel Bank Common Lisp
 (setq inferior-lisp-program "sbcl")
-;; Set SLIME
+;; slime.el
 (require 'slime)
 (slime-setup '(slime-repl slime-fancy slime-banner))
 
