6c9560e47e96968dff7a6ec5bc8429feae7a94f1
suna <sn.sable005@gmail.com>
2021-05-11 21:49:14 +0900

    brewcask.sh: add --cask --greedy

--- bin/install_macos/brewcask.sh
+++ bin/install_macos/brewcask.sh
@@ -3,7 +3,7 @@
 echo "${0} ..."
 
 # --- Upgrade(brew cask file)
-brew upgrade
+brew upgrade --cask --greedy
 
 # --- Install(brew cask file)
 brew install --cask $(grep -v -e '^$' -e '^#' $(dirname ${0})/brewcask.txt)
