e191ab2dfde9622346bbdeb54f709912656c8fd5
ghsable <sn.sable005@gmail.com>
2021-07-19 19:21:06 +0900

    shogi: add YaneuraOu

--- bin/shogi/README.md
+++ bin/shogi/README.md
@@ -2,14 +2,27 @@
 
 ## Requirement
 ### GUI
-* [将棋所](http://shogidokoro.starfree.jp/)
-  * [Mono](https://github.com/ghsable/dotfiles/blob/main/bin/apl/mono/README.md)
+| Name | Requirement | Addition |
+| :--- | :--- | :--- |
+| [将棋所](http://shogidokoro.starfree.jp/) | [Mono](https://github.com/ghsable/dotfiles/blob/main/bin/apl/mono/README.md) | `-` |
 
 ### USI shogi engine
-* [Apery](https://github.com/HiraokaTakuya/apery_rust)
-  * [Rust](https://github.com/ghsable/dotfiles/blob/main/bin/apl/rust/README.md)
-* [水匠4](https://twitter.com/tayayan_ts/status/1416621532164497411)
-  * none
+| Name | Requirement | Addition |
+| :--- | :--- | :--- |
+| [Apery](https://github.com/HiraokaTakuya/apery_rust) | [Rust](https://github.com/ghsable/dotfiles/blob/main/bin/apl/rust/README.md) | `-` |
+| [YaneuraOu](https://github.com/yaneurao/YaneuraOu) | `clang++` | [Makefile](https://github.com/ghsable/dotfiles/tree/main/bin/shogi/YaneuraOu/Makefile) <br> - build options: `YANEURAOU_ENGINE_KPPT` <br> - target cpu: `grep -i sse4_2 /proc/cpuinfo` |
+| ~~[水匠4](https://twitter.com/tayayan_ts/status/1416621532164497411)~~ | [YaneuraOu(WCSOC2020)](https://drive.google.com/drive/folders/1xMzf2NsEap8x6i5IWlzCie8enecBe7OX) <br> `g++` | `Windows only` <br> [水匠2のビルド手順](https://kisagai.com/project/engineshogibanusage/%E6%B0%B4%E5%8C%A0%EF%BC%92%E3%81%AE%E3%83%93%E3%83%AB%E3%83%89%E6%89%8B%E9%A0%86/): `build error(g++ version)` |
+
+### Evaluation function
+| Name | Requirement | Addition |
+| :--- | :--- | :--- |
+| [リゼロ評価関数 epoch8](https://github.com/yaneurao/YaneuraOu/blob/master/docs/README2017.md) | [YaneuraOu](https://github.com/yaneurao/YaneuraOu) | `-` |
+| [elmo(WCSC28)](https://mk-takizawa.github.io/elmo/) | [YaneuraOu](https://github.com/yaneurao/YaneuraOu) | `-` |
+
+### Book
+| Name | Requirement | Addition |
+| :--- | :--- | :--- |
+| [100テラショック定跡](https://github.com/yaneurao/YaneuraOu/releases/tag/BOOK-100T-Shock) | [YaneuraOu](https://github.com/yaneurao/YaneuraOu) | Use <br> - [リゼロ評価関数 epoch8](https://github.com/yaneurao/YaneuraOu/blob/master/docs/README2017.md) |
 
 ## Install/Run
 
--- bin/shogi/init.sh
+++ bin/shogi/init.sh
@@ -3,6 +3,7 @@
 readonly SHOGI_DIR=~/shogi
 [ -d ${SHOGI_DIR} ] || mkdir -pv ${SHOGI_DIR}
 [ -d ${SHOGI_DIR}/Engine ] || mkdir -pv ${SHOGI_DIR}/Engine
+[ -d ${SHOGI_DIR}/Engine/Book ] || mkdir -pv ${SHOGI_DIR}/Engine/Book
 ln -snf ~/private/Document/KIF ${SHOGI_DIR}/KIF
 
 #---- Requirement: Mono
@@ -27,20 +28,66 @@ if [ ! -d "${RUST_DIR}/apery_rust" ]; then
   ln -snfv ${RUST_DIR}/apery_rust ${SHOGI_DIR}/Engine/apery_rust
 fi
 
-#---- Requirement: none
+#---- Requirement: clang++
+# USI shogi engine - やねうら王 - https://github.com/yaneurao/YaneuraOu
+if [ ! -d "${SHOGI_DIR}/Engine/YaneuraOu" ]; then
+  cd ${SHOGI_DIR}/Engine
+  git clone --recursive https://github.com/yaneurao/YaneuraOu.git
+  cp -f ~/bin/shogi/YaneuraOu/Makefile ${SHOGI_DIR}/Engine/YaneuraOu/source/Makefile
+  cd ${SHOGI_DIR}/Engine/YaneuraOu/source
+  make
+fi
+
+# Thanks - https://www.matthuisman.nz/2019/01/download-google-drive-files-wget-curl.html
+function download_google_drive() {
+    fileid=${1}
+    readonly filename="opt.zip"
+    cd ${SHOGI_DIR}/Engine
+    curl -L -c cookies.txt 'https://docs.google.com/uc?export=download&id='$fileid \
+         | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p' >confirm.txt
+    curl -L -b cookies.txt -o $filename \
+         'https://docs.google.com/uc?export=download&id='$fileid'&confirm='$(<confirm.txt)
+    unar $filename
+    rm -f confirm.txt cookies.txt $filename
+}
+
+#---- Requirement: YaneuraOu(WCSOC2020), g++
 # USI shogi engine - 水匠 - https://twitter.com/tayayan_ts
 # 水匠4 - https://twitter.com/tayayan_ts/status/1416621532164497411
 if [ ! -d "${SHOGI_DIR}/Engine/水匠4" ]; then
-  # Thanks - https://www.matthuisman.nz/2019/01/download-google-drive-files-wget-curl.html
-  readonly fileid="1YwqmlQhfnRZDSrpISYNa4HvQaS9tEqvy"
-  readonly filename="suisho.zip"
-  cd ${SHOGI_DIR}/Engine
-  curl -L -c cookies.txt 'https://docs.google.com/uc?export=download&id='$fileid \
-       | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p' >confirm.txt
-  curl -L -b cookies.txt -o $filename \
-       'https://docs.google.com/uc?export=download&id='$fileid'&confirm='$(<confirm.txt)
-  unar $filename
-  rm -f confirm.txt cookies.txt $filename
+  download_google_drive "1YwqmlQhfnRZDSrpISYNa4HvQaS9tEqvy"
+fi
+
+#---- Requirement: YaneuraOu
+# Evaluation function - リゼロ評価関数 epoch8 - https://github.com/yaneurao/YaneuraOu/blob/master/docs/README2017.md
+if [ ! -d "${SHOGI_DIR}/Engine/epoch8" ]; then
+  download_google_drive "0Bzbi5rbfN85NMHd0OEUxcUVJQW8"
+  mv ${SHOGI_DIR}/Engine/epoch8 ${SHOGI_DIR}/Engine/eval
+  mkdir ${SHOGI_DIR}/Engine/epoch8
+  mv ${SHOGI_DIR}/Engine/eval ${SHOGI_DIR}/Engine/epoch8/eval
+  ln -snfv ${SHOGI_DIR}/Engine/YaneuraOu/source/YaneuraOu-by-gcc ${SHOGI_DIR}/Engine/epoch8/YaneuraOu-by-gcc
+  echo 'epoch8/YaneuraOu' >${SHOGI_DIR}/Engine/epoch8/engine_name.txt
+fi
+
+#---- Requirement: YaneuraOu
+# Evaluation function - elmo(WCSC28) - https://mk-takizawa.github.io/elmo/
+if [ ! -d "${SHOGI_DIR}/Engine/elmo.shogi" ]; then
+  download_google_drive "0B0XpI3oPiCmFalVGclpIZjBmdGs"
+  ln -snfv ${SHOGI_DIR}/Engine/YaneuraOu/source/YaneuraOu-by-gcc ${SHOGI_DIR}/Engine/elmo.shogi/YaneuraOu-by-gcc
+  echo 'elmo(WCSC28)/YaneuraOu' >${SHOGI_DIR}/Engine/elmo.shogi/engine_name.txt
+fi
+
+#---- Requirement: YaneuraOu
+# Book - 100テラショック定跡 - https://github.com/yaneurao/YaneuraOu/releases/tag/BOOK-100T-Shock
+if [ ! -d "${SHOGI_DIR}/Engine/Book/100T-shock-book" ]; then
+  #Thanks - https://askubuntu.com/questions/285976/download-zip-file-with-curl-command
+  cd ${SHOGI_DIR}/Engine/Book
+  curl -O -J -L https://github.com/yaneurao/YaneuraOu/releases/download/BOOK-100T-Shock/100T-shock-book.zip
+  unar 100T-shock-book.zip
+  rm 100T-shock-book.zip
+  # Evaluation function - リゼロ評価関数 epoch8 - https://github.com/yaneurao/YaneuraOu/blob/master/docs/README2017.md
+  mkdir ${SHOGI_DIR}/Engine/epoch8/book
+  ln -snfv ${SHOGI_DIR}/Engine/Book/100T-shock-book/user_book1.db ${SHOGI_DIR}/Engine/epoch8/book/standard_book.db
 fi
 
 # Run
