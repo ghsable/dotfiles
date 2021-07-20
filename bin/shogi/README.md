62a75fafab059769860d458149775227364828a6
ghsable <sn.sable005@gmail.com>
2021-07-21 00:06:19 +0900

    shogi: Add KPPT/NNUE

--- bin/shogi/README.md
+++ bin/shogi/README.md
@@ -7,22 +7,39 @@
 | [将棋所](http://shogidokoro.starfree.jp/) | [Mono](https://github.com/ghsable/dotfiles/blob/main/bin/apl/mono/README.md) | `-` |
 
 ### USI shogi engine
+#### KPPT
 | Name | Requirement | Addition |
 | :--- | :--- | :--- |
 | [Apery](https://github.com/HiraokaTakuya/apery_rust) | [Rust](https://github.com/ghsable/dotfiles/blob/main/bin/apl/rust/README.md) | `-` |
-| [YaneuraOu](https://github.com/yaneurao/YaneuraOu) | `clang++` | [Makefile](https://github.com/ghsable/dotfiles/tree/main/bin/shogi/YaneuraOu/Makefile) <br> - build options: `YANEURAOU_ENGINE_KPPT` <br> - target cpu: `grep -i sse4_2 /proc/cpuinfo` |
-| ~~[水匠4](https://twitter.com/tayayan_ts/status/1416621532164497411)~~ | [YaneuraOu(WCSOC2020)](https://drive.google.com/drive/folders/1xMzf2NsEap8x6i5IWlzCie8enecBe7OX) <br> `g++` | `Windows only` <br> [水匠2のビルド手順](https://kisagai.com/project/engineshogibanusage/%E6%B0%B4%E5%8C%A0%EF%BC%92%E3%81%AE%E3%83%93%E3%83%AB%E3%83%89%E6%89%8B%E9%A0%86/): `build error(g++ version)` |
+| [YaneuraOu](https://github.com/yaneurao/YaneuraOu) | `clang++` | [Makefile](https://github.com/ghsable/dotfiles/tree/main/bin/shogi/YaneuraOu/KPPT/Makefile) <br> - build options: `YANEURAOU_ENGINE_KPPT` <br> - target cpu: `grep -i sse4_2 /proc/cpuinfo` |
+
+#### NNUE
+| Name | Requirement | Addition |
+| :--- | :--- | :--- |
+| [YaneuraOu](https://github.com/yaneurao/YaneuraOu) | `clang++` | [Makefile](https://github.com/ghsable/dotfiles/tree/main/bin/shogi/YaneuraOu/NNUE/Makefile) <br> - build options: `YANEURAOU_ENGINE_NNUE` <br> - target cpu: `grep -i sse4_2 /proc/cpuinfo` |
 
 ### Evaluation function
+#### KPPT
 | Name | Requirement | Addition |
 | :--- | :--- | :--- |
 | [リゼロ評価関数 epoch8](https://github.com/yaneurao/YaneuraOu/blob/master/docs/README2017.md) | [YaneuraOu](https://github.com/yaneurao/YaneuraOu) | `-` |
-| [elmo(WCSC28)](https://mk-takizawa.github.io/elmo/) | [YaneuraOu](https://github.com/yaneurao/YaneuraOu) | `-` |
+| [elmo_WCSC28](https://mk-takizawa.github.io/elmo/) | [YaneuraOu](https://github.com/yaneurao/YaneuraOu) | `-` |
+
+#### NNUE
+| Name | Requirement | Addition |
+| :--- | :--- | :--- |
+| [水匠4](https://twitter.com/tayayan_ts/status/1416621532164497411) | [YaneuraOu](https://github.com/yaneurao/YaneuraOu) | `-` |
 
 ### Book
+#### KPPT
 | Name | Requirement | Addition |
 | :--- | :--- | :--- |
 | [100テラショック定跡](https://github.com/yaneurao/YaneuraOu/releases/tag/BOOK-100T-Shock) | [YaneuraOu](https://github.com/yaneurao/YaneuraOu) | Use <br> - [リゼロ評価関数 epoch8](https://github.com/yaneurao/YaneuraOu/blob/master/docs/README2017.md) |
+
+#### NNUE
+| Name | Requirement | Addition |
+| :--- | :--- | :--- |
+| `-` | `-` | `-` |
 
 ## Install/Run
 
--- bin/shogi/init.sh
+++ bin/shogi/init.sh
@@ -1,48 +1,52 @@
 #!/bin/bash
 
-readonly SHOGI_DIR=~/shogi
-[ -d ${SHOGI_DIR} ] || mkdir -pv ${SHOGI_DIR}
-[ -d ${SHOGI_DIR}/Engine ] || mkdir -pv ${SHOGI_DIR}/Engine
-[ -d ${SHOGI_DIR}/Engine/Book ] || mkdir -pv ${SHOGI_DIR}/Engine/Book
-ln -snf ~/private/Document/KIF ${SHOGI_DIR}/KIF
+readonly KIF_DIR=~/shogi/KIF
+ln -snf ~/private/Document/KIF ${KIF_DIR}
+
+readonly KPPT_DIR=~/shogi/Engine/KPPT
+readonly NNUE_DIR=~/shogi/Engine/NNUE
+readonly BOOK_DIR=~/shogi/Engine/BOOK
+[ -d ${KPPT_DIR} ] || mkdir -pv ${KPPT_DIR}
+[ -d ${NNUE_DIR} ] || mkdir -pv ${NNUE_DIR}
+[ -d ${BOOK_DIR} ] || mkdir -pv ${BOOK_DIR}
+
+# ---------- GUI ---------- #
 
 #---- Requirement: Mono
 # GUI - Shogidokoro - http://shogidokoro.starfree.jp/
-if [ ! -d "${SHOGI_DIR}/Shogidokoro" ]; then
-  cd ${SHOGI_DIR}
+if [ ! -d ~/shogi/Shogidokoro ]; then
+  cd ~/shogi
   curl -O http://shogidokoro.starfree.jp/download/Shogidokoro.zip
   unar Shogidokoro.zip
   rm Shogidokoro.zip
 fi
 
+# ---------- KPPT ---------- #
+
 #---- Requirement: Rust
 # USI shogi engine - apery_rust - https://github.com/HiraokaTakuya/apery_rust
-readonly RUST_DIR=~/rust
-[ -d ${RUST_DIR} ] || mkdir -pv ${RUST_DIR}
-if [ ! -d "${RUST_DIR}/apery_rust" ]; then
-  cd ${RUST_DIR}
+if [ ! -d "${KPPT_DIR}/apery_rust" ]; then
+  cd ${KPPT_DIR}
   git clone --recursive https://github.com/HiraokaTakuya/apery_rust.git
-  cd ${RUST_DIR}/apery_rust
+  cd ${KPPT_DIR}/apery_rust
   cargo build --release
-  ln -snfv ${RUST_DIR}/apery_rust/eval ${RUST_DIR}/apery_rust/target/release/eval
-  ln -snfv ${RUST_DIR}/apery_rust ${SHOGI_DIR}/Engine/apery_rust
+  ln -snfv ${KPPT_DIR}/apery_rust/eval ${KPPT_DIR}/apery_rust/target/release/eval
 fi
 
 #---- Requirement: clang++
 # USI shogi engine - やねうら王 - https://github.com/yaneurao/YaneuraOu
-if [ ! -d "${SHOGI_DIR}/Engine/YaneuraOu" ]; then
-  cd ${SHOGI_DIR}/Engine
+if [ ! -d "${KPPT_DIR}/YaneuraOu" ]; then
+  cd ${KPPT_DIR}
   git clone --recursive https://github.com/yaneurao/YaneuraOu.git
-  cp -f ~/bin/shogi/YaneuraOu/Makefile ${SHOGI_DIR}/Engine/YaneuraOu/source/Makefile
-  cd ${SHOGI_DIR}/Engine/YaneuraOu/source
+  cp -f ~/bin/shogi/YaneuraOu/KPPT/Makefile ${KPPT_DIR}/YaneuraOu/source/Makefile
+  cd ${KPPT_DIR}/YaneuraOu/source
   make
 fi
 
 # Thanks - https://www.matthuisman.nz/2019/01/download-google-drive-files-wget-curl.html
 function download_google_drive() {
-    fileid=${1}
+    fileid=$1
     filename="opt.zip"
-    cd ${SHOGI_DIR}/Engine
     curl -L -c cookies.txt 'https://docs.google.com/uc?export=download&id='$fileid \
          | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p' >confirm.txt
     curl -L -b cookies.txt -o $filename \
@@ -51,44 +55,62 @@ function download_google_drive() {
     rm -f confirm.txt cookies.txt $filename
 }
 
-#---- Requirement: YaneuraOu(WCSOC2020), g++
-# USI shogi engine - 水匠 - https://twitter.com/tayayan_ts
-# 水匠4 - https://twitter.com/tayayan_ts/status/1416621532164497411
-if [ ! -d "${SHOGI_DIR}/Engine/水匠4" ]; then
-  download_google_drive "1YwqmlQhfnRZDSrpISYNa4HvQaS9tEqvy"
-fi
-
-#---- Requirement: YaneuraOu
+#---- Requirement: YaneuraOu_KPPT
 # Evaluation function - リゼロ評価関数 epoch8 - https://github.com/yaneurao/YaneuraOu/blob/master/docs/README2017.md
-if [ ! -d "${SHOGI_DIR}/Engine/epoch8" ]; then
+if [ ! -d "${KPPT_DIR}/epoch8" ]; then
+  cd ${KPPT_DIR}
   download_google_drive "0Bzbi5rbfN85NMHd0OEUxcUVJQW8"
-  mv ${SHOGI_DIR}/Engine/epoch8 ${SHOGI_DIR}/Engine/eval
-  mkdir ${SHOGI_DIR}/Engine/epoch8
-  mv ${SHOGI_DIR}/Engine/eval ${SHOGI_DIR}/Engine/epoch8/eval
-  ln -snfv ${SHOGI_DIR}/Engine/YaneuraOu/source/YaneuraOu-by-gcc ${SHOGI_DIR}/Engine/epoch8/YaneuraOu-by-gcc
-  echo 'epoch8/YaneuraOu_V6.00_KPPT/100T-shock' >${SHOGI_DIR}/Engine/epoch8/engine_name.txt
+  mv ${KPPT_DIR}/epoch8 ${KPPT_DIR}/eval
+  mkdir ${KPPT_DIR}/epoch8
+  mv ${KPPT_DIR}/eval ${KPPT_DIR}/epoch8/eval
+  ln -snfv ${KPPT_DIR}/YaneuraOu/source/YaneuraOu-by-gcc ${KPPT_DIR}/epoch8/YaneuraOu-by-gcc
+  echo 'epoch8/YaneuraOu_V6.00_KPPT/100T-shock' >${KPPT_DIR}/epoch8/engine_name.txt
 fi
 
-#---- Requirement: YaneuraOu
-# Evaluation function - elmo(WCSC28) - https://mk-takizawa.github.io/elmo/
-if [ ! -d "${SHOGI_DIR}/Engine/elmo.shogi" ]; then
+#---- Requirement: YaneuraOu_KPPT
+# Evaluation function - elmo_WCSC28 - https://mk-takizawa.github.io/elmo/
+if [ ! -d "${KPPT_DIR}/elmo.shogi" ]; then
+  cd ${KPPT_DIR}
   download_google_drive "0B0XpI3oPiCmFalVGclpIZjBmdGs"
-  ln -snfv ${SHOGI_DIR}/Engine/YaneuraOu/source/YaneuraOu-by-gcc ${SHOGI_DIR}/Engine/elmo.shogi/YaneuraOu-by-gcc
-  echo 'elmo_WCSC28/YaneuraOu_V6.00_KPPT/100T-shock' >${SHOGI_DIR}/Engine/elmo.shogi/engine_name.txt
+  ln -snfv ${KPPT_DIR}/YaneuraOu/source/YaneuraOu-by-gcc ${KPPT_DIR}/elmo.shogi/YaneuraOu-by-gcc
+  echo 'elmo_WCSC28/YaneuraOu_V6.00_KPPT' >${KPPT_DIR}/elmo.shogi/engine_name.txt
 fi
+
+# ---------- BOOK ---------- #
 
 #---- Requirement: YaneuraOu
 # Book - 100テラショック定跡 - https://github.com/yaneurao/YaneuraOu/releases/tag/BOOK-100T-Shock
-if [ ! -d "${SHOGI_DIR}/Engine/Book/100T-shock-book" ]; then
+if [ ! -d "${BOOK_DIR}/100T-shock-book" ]; then
   #Thanks - https://askubuntu.com/questions/285976/download-zip-file-with-curl-command
-  cd ${SHOGI_DIR}/Engine/Book
+  cd ${BOOK_DIR}
   curl -O -J -L https://github.com/yaneurao/YaneuraOu/releases/download/BOOK-100T-Shock/100T-shock-book.zip
   unar 100T-shock-book.zip
   rm 100T-shock-book.zip
   # Evaluation function - リゼロ評価関数 epoch8 - https://github.com/yaneurao/YaneuraOu/blob/master/docs/README2017.md
-  mkdir ${SHOGI_DIR}/Engine/epoch8/book
-  ln -snfv ${SHOGI_DIR}/Engine/Book/100T-shock-book/user_book1.db ${SHOGI_DIR}/Engine/epoch8/book/standard_book.db
+  mkdir -pv ${KPPT_DIR}/epoch8/book
+  ln -snfv ${BOOK_DIR}/100T-shock-book/user_book1.db ${KPPT_DIR}/epoch8/book/standard_book.db
+fi
+
+# ---------- NNUE ---------- #
+
+#---- Requirement: clang++
+# USI shogi engine - やねうら王 - https://github.com/yaneurao/YaneuraOu
+if [ ! -d "${NNUE_DIR}/YaneuraOu" ]; then
+  cd ${NNUE_DIR}
+  git clone --recursive https://github.com/yaneurao/YaneuraOu.git
+  cp -f ~/bin/shogi/YaneuraOu/NNUE/Makefile ${NNUE_DIR}/YaneuraOu/source/Makefile
+  cd ${NNUE_DIR}/YaneuraOu/source
+  make
+fi
+
+#---- Requirement: YaneuraOu_NNUE
+# USI shogi engine - 水匠 - https://twitter.com/tayayan_ts
+# 水匠4 - https://twitter.com/tayayan_ts/status/1416621532164497411
+if [ ! -d "${NNUE_DIR}/水匠4" ]; then
+  cd ${NNUE_DIR}
+  download_google_drive "1YwqmlQhfnRZDSrpISYNa4HvQaS9tEqvy"
+  ln -snfv ${NNUE_DIR}/YaneuraOu/source/YaneuraOu-by-gcc ${NNUE_DIR}/水匠4/YaneuraOu-by-gcc
 fi
 
 # Run
-mono ${SHOGI_DIR}/Shogidokoro/Shogidokoro.exe
+mono ~/shogi/Shogidokoro/Shogidokoro.exe
